75bc094c57647b9bc9ed55685df5da40f53e5c4c
Michele Murakami <mmurakam@ucsd.edu>
2020-01-30 10:05:01 -0800

    changes to hello

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var project = require('./routes/project');
 // var user = require('./routes/user');
 
 var app = express();
+var hello = require('./routes/hello');
+app.get('/hello/:userName', hello.view)
 
 // all environments
 app.set('port', process.env.PORT || 3000);
